babbe93e3552002866125e1131d5acd8f5bcedf8
Saketh Sampath Vinay <sakethsampthvinay46@gmail.com>
2025-06-15 20:51:26 +0530

    fixed auth

--- src/app/login/LoginClient.tsx
+++ src/app/login/LoginClient.tsx
@@ -150,11 +150,11 @@ export default function LoginClient() {
               <div className="absolute inset-0 flex items-center">
                 <span className="w-full border-t" />
               </div>
-              <div className="relative flex justify-center text-xs uppercase">
+              {/* <div className="relative flex justify-center text-xs uppercase">
                 <span className="bg-background px-2 text-muted-foreground">Or continue with</span>
-              </div>
+              </div> */}
             </div>
-            <Button
+            {/* <Button
               variant="outline"
               className="w-full mt-4 flex items-center justify-center gap-2"
               onClick={handleGoogleSignInClick}
@@ -162,7 +162,7 @@ export default function LoginClient() {
             >
               <GoogleLogo />
               Sign in with Google
-            </Button>
+            </Button> */}
           </div>
         </CardContent>
         <CardFooter className="flex justify-center">
